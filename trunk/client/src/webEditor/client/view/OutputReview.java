8a90bffc297d21a27b910aedb2f7335659f8dc48
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2011-06-14 19:38:22 +0000

    urgh.  I hate uibinder.

---
+++ trunk/client/src/webEditor/client/view/OutputReview.java
@@ -0,0 +1,47 @@
+package webEditor.client.view;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasText;
+import com.google.gwt.user.client.ui.Widget;
+
+public class OutputReview extends Composite implements HasText {
+
+	private static OutputReviewUiBinder uiBinder = GWT
+			.create(OutputReviewUiBinder.class);
+
+	interface OutputReviewUiBinder extends UiBinder<Widget, OutputReview> {
+	}
+
+	public OutputReview() {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+	
+
+	public OutputReview(String firstName) {
+		initWidget(uiBinder.createAndBindUi(this));
+	}
+
+
+
+	@Override
+	public String getText() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public void setText(String text) {
+		// TODO Auto-generated method stub
+		
+	}
+}
