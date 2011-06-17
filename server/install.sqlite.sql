c975d2ef565985b8a363a9f2c4e90bdf9cf4cda7
bostrt <bostrt@gmail.com>
2011-06-17 21:06:07 +0000

    - Added tables for exercises and submissions.

--- server/install.sqlite.sql
+++ server/install.sqlite.sql
@@ -18,3 +18,21 @@ CREATE TABLE file (
        updated INT NOT NULL,
        added INT NOT NULL
 );
+
+CREATE TABLE exercise (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       title VARCHAR(255) NOT NULL,
+       description TEXT,
+       skeleton TEXT,
+       solution TEXT,   
+       added INT NOT NULL,
+       updated INT NOT NULL
+);
+
+CREATE TABLE submission (
+       id INTEGER PRIMARY KEY AUTOINCREMENT,
+       code TEXT NOT NULL,
+       exerciseId NOT NULL REFERENCES exercise(id),
+       studentId NOT NULL REFERENCES user(id),
+       added INT NOT NULL
+);
