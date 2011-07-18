f3494e17360fa5ae4ad4aad44abb6be9483a270c
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2011-07-18 16:50:09 +0000

    About to add "success" column to "submissions" table and propogated alterations.  Fail-safe commit.

--- client/src/webEditor/client/view/Admin.java
+++ client/src/webEditor/client/view/Admin.java
@@ -1,26 +1,33 @@
 package webEditor.client.view;
 
+import java.util.HashMap;
+
 import webEditor.client.Proxy;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.CheckBox;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FileUpload;
 import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.ListBox;
 import com.google.gwt.user.client.ui.SubmitButton;
 import com.google.gwt.user.client.ui.TextArea;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteEvent;
 import com.google.gwt.user.client.ui.FormPanel.SubmitCompleteHandler;
-import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
-import com.google.gwt.user.client.ui.FormPanel.SubmitHandler;
 
 public class Admin extends Composite{
 
+	private HashMap<String, String> exerciseMap = new HashMap<String, String>();
+	
 	@UiField FileUpload solution;
 	@UiField FileUpload skeleton;
 	@UiField TextArea desc;
@@ -28,6 +35,9 @@ public class Admin extends Composite{
 	@UiField FormPanel adminForm;
 	@UiField TextBox fileName;
 	@UiField CheckBox visible;
+	@UiField ListBox exercises;
+	@UiField Button btnAdminReview;
+	@UiField Grid grdAdminReview;
 	
 	private static AdminUiBinder uiBinder = GWT.create(AdminUiBinder.class);
 
@@ -36,10 +46,8 @@ public class Admin extends Composite{
 
 	public Admin() {
 		initWidget(uiBinder.createAndBindUi(this));
-		
-		final int isVisible;
-		if (visible.isEnabled()) isVisible = 1;
-		else isVisible = 0;
+
+		Proxy.getVisibleExercises(exercises, exerciseMap); 
 		
 		adminForm.setAction(Proxy.getBaseURL() + "?cmd=AddExercise");
 		adminForm.setEncoding(FormPanel.ENCODING_MULTIPART);
@@ -63,6 +71,13 @@ public class Admin extends Composite{
 			}
 			
 		});
+	}
+	
+	@UiHandler("btnAdminReview")
+	void onReviewClick(ClickEvent event)
+	{
+		String value = exercises.getValue(exercises.getSelectedIndex());
+		Window.alert(exerciseMap.get(value));
 	}
 
 }
