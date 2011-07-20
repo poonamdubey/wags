612f82455ada3084451f3cbe25c4aaa54caf9793
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2011-07-20 17:14:36 +0000

    Added run code, but it doesn't work properly - php needs to be configured?

--- trunk/server/class/command/AddExercise.php
+++ trunk/server/class/command/AddExercise.php
@@ -18,57 +18,58 @@ class AddExercise extends Command
         if(!Auth::isLoggedIn()){
             return JSON::error('Must be logged in as administrator
                 to log an exercise');   
-        }
+	}
 
-        //Grab solution
-        if(!isset($_FILES['Solution'])){
-            return JSON::error('No solution');
-        }
-
-        $solution = $_FILES['Solution'];
-
-        //Grab skeleton
-        if(!isset($_FILES['Skeleton'])){
-            return JSON::error('No skeleton');
-        }
-        
-        $skeleton = $_FILES['Skeleton'];
-
-        //check both files for plain text
-        $finfo = finfo_open(FILEINFO_MIME_TYPE);
-        $type = finfo_file($finfo, $solution['tmp_name']);
-        if(strpos($type, 'text') === FALSE){
-            return JSON::error('Please only upload plain text or source files (solution)');
-        }
-        
-        $type = finfo_file($finfo, $skeleton['tmp_name']);
-        if(strpos($type, 'text') === FALSE){
-            return JSON::error('Please only upload plain text or source files (skeleton)');
-        }
-
-        $solutionContents = file_get_contents($solution['tmp_name']);
-        $skeletonContents = file_get_contents($skeleton['tmp_name']);
-	$description = $_POST['desc'];
 	$name = $_POST['fileName'];
+
+	if(Exercise::exerciseExistsByTitle($name)){
+		$e = Exercise::getExerciseByTitle($name);
+		$update = true;
+	}else{
+        	//Grab solution
+        	if(!isset($_FILES['Solution'])){
+        	    return JSON::error('No solution');
+	        }	
+
+        	$solution = $_FILES['Solution'];
+
+	        //Grab skeleton
+	        if(!isset($_FILES['Skeleton'])){
+	            return JSON::error('No skeleton');
+	        }
+        
+      		$skeleton = $_FILES['Skeleton'];
+
+        	//check both files for plain text
+        	$finfo = finfo_open(FILEINFO_MIME_TYPE);
+       		$type = finfo_file($finfo, $solution['tmp_name']);
+	        if(strpos($type, 'text') === FALSE){
+           	    return JSON::error('Please only upload plain text or source files (solution)');
+       		}
+        
+        	$type = finfo_file($finfo, $skeleton['tmp_name']);
+        	if(strpos($type, 'text') === FALSE){
+        	    return JSON::error('Please only upload plain text or source files (skeleton)');
+       		}	
+
+       		$solutionContents = file_get_contents($solution['tmp_name']);
+       	 	$skeletonContents = file_get_contents($skeleton['tmp_name']);
+		$description = $_POST['desc'];
+
+		$e = new Exercise;
+                $e->setSolution($solutionContents);
+                $e->setSkeleton($skeletonContents);
+                $e->setDescription($description);
+                $e->setTitle($name);
+                $e->setAdded(time());
+	}
+
 	$visible = $_POST['visible'];
 
 	if($visible == "on") $visible = 1;
 	else $visible = 0;
 
-	if(Exercise::exerciseExistsByTitle($name)){
-		$e = Exercise::getExerciseByTitle($name);
-		$update = true;
-    	} else {
-		$e = new Exercise();
-		$e->setSolution($solutionContents);
-		$e->setSkeleton($skeletonContents);
-		$e->setDescription($description);
-		$e->setTitle($name);
-		$e->setAdded(time());
-	}
-
 	$e->setVisible($visible);
-
 	$now = time();
 	$e->setUpdated($now);	
 
---
+++ trunk/server/class/command/runcode.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Since you cannot use pcntl_fork()
+ * from within an Apache PHP script
+ * we'll do this. 
+ *
+ * This script will be called from class/command/Review.php.
+ * 
+ * Spawn a process. Parent will wait around for about WAIT_TIME seconds
+ * and if it has been longer than WAIT_TIME seconds and the child is not 
+ * done running KILL IT! 
+ */
+
+define('WAIT_TIME', 1);
+
+/* Get arguments passed in */
+$dir = $argv[1];
+$className = $argv[2];
+
+/* fork it! */
+$pid = pcntl_fork();
+
+if($pid == -1){
+    echo 'Could not run code';
+}
+else if($pid == 0){
+    exec("/usr/bin/java -cp $dir $className 2>&1", $output);
+    print_r($output[0]);
+    return;
+}
+else{
+    /* 
+     * Manage group.
+     * Wait WAIT_TIME seconds
+     */
+    $now = time()+WAIT_TIME;
+    $result = null;
+    while(time() <= $now){
+        $result = pcntl_waitpid($pid, $status, WNOHANG);
+    }
+    print $result.' '.$status;
+    if($result != -1){
+        /* Exited child ID */
+        print $result;
+    }else{
+        /* Kill the group */
+        $pgid = posix_getpgid($pid);
+	exec("kill -9 -$pgid");  //responsible for printing
+				//kill: #: Illegal number:
+    }
+}
+
+?>
