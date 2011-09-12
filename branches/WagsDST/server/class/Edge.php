340e0e40d0f341dbb661bd7392fa3f74f9411fe3
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2011-09-12 16:55:25 +0000

    Creating a branch for implementing DST

---
+++ branches/WagsDST/server/class/Edge.php
@@ -0,0 +1,64 @@
+<?php
+
+require_once("Model.php");
+
+class Edge extends Model
+{
+	protected $problemResult;
+	protected $node1Key;
+	protected $node2Key;
+
+	//Getters
+	public function getProblemResult(){
+		return $this->problemResult;
+	}
+
+	//Node1 and Node2 should hold the
+	//key for the node, rather than the
+	//id, as keys for nodes should be distinct
+	//(at least for now...)
+	public function getNode1Key(){
+		return $this->node1Key;
+	}
+
+	public function getNode2Key(){
+		return $this->node2Key;
+	}
+
+	public function getNode1(){
+		require_once('Database.php');
+		$db = Database::getDb();
+
+		$sth = $db->prepare('SELECT * FROM Node WHERE problemResult = :probResult AND
+			Key LIKE :key');
+		$sth->execute(array(':probResult' => $this->problemResult, 
+			':key' => $this->node1Key));
+
+		return $sth->fetchObject('Node');
+	}
+
+	public function getNode2(){
+		require_once('Database.php');
+		$db = Database::getDb();
+
+		$sth = $db->prepare('SELECT * FROM Node WHERE problemResult = :probResult AND
+			Key LIKE :key');
+		$sth->execute(array(':probResult' => $this->problemResult, 
+			':key' => $this->node2Key));
+
+		return $sth->fetchObject('Node');
+	}
+
+		//Setters
+	public function setProblemResult($probResult){
+		$this->problemResult = $probResult;
+	}
+
+	public function setNode1($newNode){
+		$this->node1 = $newNode;
+	}
+
+	public function setNode2($newNode){
+		$this->node2 = $newNode;
+	}
+}
