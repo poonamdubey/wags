2f8fcf238f1b1c6821bbf86608b133333d04a3e0
pmeznar <pmeznar@062e9602-64fa-fb65-0213-a643acedf34c>
2011-09-15 16:11:58 +0000

    All the DST stuff drawn in.  Woooo.  Of course it is all borken.

---
+++ client/src/dst/logic/AvailableServices.java
@@ -0,0 +1,8 @@
+package dst.logic;
+
+public interface AvailableServices {
+	
+	public static final int GET_EMAIL_ADDR_AND_LOGOUT_URL = 0;
+	public static final int GET_REPORTS = 1;
+	public static final int PROBLEM_ID_OFFSET = 2;
+}
