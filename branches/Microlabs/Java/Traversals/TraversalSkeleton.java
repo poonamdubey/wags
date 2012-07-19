ad065e674edad002e0b2390959e3e82cbbacd2dd
mdusen9@gmail.com <mdusen9@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2012-07-19 18:31:16 +0000

    Adding the Java Traversal exercises to the "Microlabs" repository
    -These are probably old versions, but always work when testing
      system code.
    -The Traversals_Nested_Packages exercise makes use of the new
      support for nested package statements

---
+++ branches/Microlabs/Java/Traversals/TraversalSkeleton.java
@@ -0,0 +1,76 @@
+/**
+ * @author: Philip Meznar
+ * @date: Aug 22, 2011
+ * @file: TraversalSkeleton.java
+ * 
+ * Description: The skeleton class
+ * to be used with WAGS for the 
+ * traversal exercises
+ */
+package traversals;
+
+public class TraversalSkeleton {
+	
+//<end!TopSection>	
+	/**
+	 * Each of these methods takes the root
+	 * node of a tree, and should return
+	 * a String showing the respective traversal
+	 * @param tree
+	 */
+    String preorderTraversal(Node tree){
+	String traversal = "";
+    	
+        if(tree != null){
+            traversal += tree.key;
+            traversal += preorderTraversal(tree.left);
+            traversal += preorderTraversal(tree.right);
+        }
+        
+        return traversal;      
+    }
+
+    String inorderTraversal(Node tree){
+    
+        String traversal = "";
+    	
+        if(tree != null){
+            traversal += inorderTraversal(tree.left);
+            traversal += tree.key;
+            traversal += inorderTraversal(tree.right);
+        }
+        
+        return traversal;      
+    }
+
+    String postorderTraversal(Node tree){
+      
+        String traversal = "";
+    	
+        if(tree != null){
+            traversal += postorderTraversal(tree.left);
+            traversal += postorderTraversal(tree.right);
+            traversal += tree.key;
+        }
+        
+        return traversal;     
+    }
+//<end!MidSection>
+
+
+    public class Node {
+        char key;
+        Node left, right;
+        public Node(char ch){
+            key = ch; 
+            left = null;
+            right = null;
+        }   
+    
+        public char getKey() { return key; }
+        public Node getLeft() { return left; }
+        public Node getRight() { return right; }
+        public void setLeft(Node l) { left = l; }
+        public void setRight(Node r) {right = r; }
+    }  
+}
