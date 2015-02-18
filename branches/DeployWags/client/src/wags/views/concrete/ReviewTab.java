373641cf17e98ae769f43a1482649e07a15a81b0
scottshuffler@gmail.com <scottshuffler@gmail.com@062e9602-64fa-fb65-0213-a643acedf34c>
2015-02-18 14:45:02 +0000

    Few gui changes

--- branches/DeployWags/client/src/wags/views/concrete/ReviewTab.java
+++ branches/DeployWags/client/src/wags/views/concrete/ReviewTab.java
@@ -12,7 +12,10 @@ import wags.admin.ReviewPanel;
 import wags.presenters.interfaces.ReviewTabPresenter;
 import wags.views.interfaces.ReviewTabView;
 
+import com.github.gwtbootstrap.client.ui.ListBox;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
@@ -31,65 +34,134 @@ public class ReviewTab extends Composite implements ReviewTabView{
 	@UiField ReviewPanel rvLogPanel, rvMagPanel;
 	@UiField SubmitButton btnCompReview;
 	@UiField FormPanel formCompReview;
-	Reviewer logHandler, magHandler;
+	@UiField ListBox magReview, logReview, magReviewPast, logReviewPast;
+	Reviewer logHandler, magHandler, magRev, logRev;
+	
 	
 	private ReviewTabPresenter presenter;
 
 	public ReviewTab() {
 		initWidget(uiBinder.createAndBindUi(this));
-		logHandler = new LogicalReviewHandler();
-		magHandler = new MagnetReviewHandler();
+		logRev = new LogicalReviewHandler();
+		magRev = new MagnetReviewHandler();
+		
 		
 		rvLogPanel.setParent(logHandler);
-		rvLogPanel.setTitle("Logical Review");
 		rvMagPanel.setParent(magHandler);
-		rvMagPanel.setTitle("Magnet Review");
 		
-		AbstractServerCall cmd1 = new GetLMAssigned(logHandler, Reviewer.NONE);
+		AbstractServerCall cmd1 = new GetLMAssigned(logRev, Reviewer.NONE);
 		cmd1.sendRequest();
-		//Proxy.getLMAssigned(logHandler);
 		
-		AbstractServerCall cmd2 = new GetMMAssignedCommand(magHandler, Reviewer.NONE);
+		AbstractServerCall cmd2 = new GetMMAssignedCommand(magRev, Reviewer.NONE);
 		cmd2.sendRequest();
 		
-		//Proxy.getMMAssigned(magHandler, Reviewer.NONE);
-		
-		AbstractServerCall cmd3 = new GetLMAssigned(logHandler, Reviewer.GET_REVIEW);
+		AbstractServerCall cmd3 = new GetLMAssigned(logRev, Reviewer.GET_REVIEW);
 		cmd3.sendRequest();
-		//Proxy.getLMAssigned(logHandler, Reviewer.GET_REVIEW);
 		
-		AbstractServerCall cmd4 = new GetMMAssignedCommand(logHandler, Reviewer.GET_REVIEW);
+		AbstractServerCall cmd4 = new GetMMAssignedCommand(magRev, Reviewer.GET_REVIEW);
 		cmd4.sendRequest();
-		//Proxy.getMMAssigned(magHandler, Reviewer.GET_REVIEW);
 		
 		btnCompReview.addStyleName("button");
 		
 		formCompReview.setAction(Proxy.getBaseURL()+"?cmd=ComprehensiveReview");
 		formCompReview.setEncoding(FormPanel.ENCODING_MULTIPART);
 		formCompReview.setMethod(FormPanel.METHOD_POST);
+		
+		
+		//Click handlers
+		logReview.addChangeHandler(new ChangeHandler()
+		 {
+		  @Override
+		public void onChange(ChangeEvent event)
+		  {
+			  int selectedIndex = logReview.getSelectedIndex();
+			  if (selectedIndex > -1) 
+			  {
+				  logRev.review(logReview.getValue(selectedIndex));
+		      }
+		  }
+		 });
+		//Click handlers
+		logReviewPast.addChangeHandler(new ChangeHandler()
+		 {
+		  @Override
+		public void onChange(ChangeEvent event)
+		  {
+			  int selectedIndex = logReviewPast.getSelectedIndex();
+			  if (selectedIndex > -1) 
+			  {
+				  logRev.review(logReviewPast.getValue(selectedIndex));
+		      }
+		  }
+		 });
+		//Click handlers
+		magReview.addChangeHandler(new ChangeHandler()
+		 {
+		  @Override
+		public void onChange(ChangeEvent event)
+		  {
+			  int selectedIndex = magReview.getSelectedIndex();
+			  if (selectedIndex > -1) 
+			  {
+				  magRev.review(magReview.getValue(selectedIndex));
+		      }
+		  }
+		 });
+		//Click handlers
+		magReviewPast.addChangeHandler(new ChangeHandler()
+		 {
+		  @Override
+		public void onChange(ChangeEvent event)
+		  {
+			  int selectedIndex = magReviewPast.getSelectedIndex();
+			  if (selectedIndex > -1) 
+			  {
+				  magRev.review(magReviewPast.getValue(selectedIndex));
+		      }
+		  }
+		 });
 	}
-
+	
+	//Fills drop down menus
 	public class LogicalReviewHandler implements Reviewer{
 		@Override
 		public void getCallback(String[] exercises, WEStatus status,
-				String request) { 
-			// Currently assigned
-			if(request.equals("")){
-				rvLogPanel.setCurrent(exercises);
-				
+				String request) {
+			String[] assigned = new String[exercises.length];
+			String[] prevAssigned = new String[exercises.length];
+			
+			for (int i =0; i < exercises.length;i++)
+			{
+				if(request.equals("")){
+					assigned[i] = exercises[i];
+				}
+				if(request.equals(GET_REVIEW)){
+					prevAssigned[i] = exercises[i];
+				}
 			}
 			
-			// Review
-			if(request.equals(GET_REVIEW)){
-				rvLogPanel.setReview(exercises);
+			for (int i = 0; i < assigned.length; i++)
+			{
+				if (assigned[i] != null)
+				{
+					logReview.addItem(assigned[i]);
+				}
 			}
+			
+			for (int i = 0; i < prevAssigned.length; i++)
+			{
+				if (prevAssigned[i] != null)
+				{
+					logReviewPast.addItem(prevAssigned[i]);
+				}			
+			}
+			logRev.review(logReview.getValue(0));
 		}
 
 		@Override
 		public void review(String exercise) {
 			AbstractServerCall cmd = new ReviewExerciseCommand(exercise, LOGICAL, this);
 			cmd.sendRequest();
-			//Proxy.reviewExercise(exercise, LOGICAL, this);
 		}
 
 		@Override
@@ -99,26 +171,46 @@ public class ReviewTab extends Composite implements ReviewTabView{
 		
 	}
 	
+	//Fills drop down menus
 	public class MagnetReviewHandler implements Reviewer{
 		@Override
 		public void getCallback(String[] exercises, WEStatus status,
 				String request) { 
-			// Currently assigned
-						if(request.equals("")){
-							rvMagPanel.setCurrent(exercises);
-						}
-						
-						// Review
-						if(request.equals(GET_REVIEW)){
-							rvMagPanel.setReview(exercises);
-						}
+			String[] magAssigned = new String[exercises.length];
+			String[] magPrevAssigned = new String[exercises.length];
+			for (int i =0; i < exercises.length;i++)
+			{
+				if(request.equals("")){
+					magAssigned[i] = exercises[i];
+				}
+				
+				if(request.equals(GET_REVIEW)){
+					magPrevAssigned[i] = exercises[i];
+				}
+			}
+			
+			for (int i = 0; i < magAssigned.length; i++)
+			{
+				if (magAssigned[i] != null)
+				{
+					magReview.addItem(magAssigned[i]);
+				}
+			}
+			
+			for (int i = 0; i < magPrevAssigned.length; i++)
+			{
+				if (magPrevAssigned[i] != null)
+				{
+					magReviewPast.addItem(magPrevAssigned[i]);
+				}
+			}
+			magRev.review(magReview.getValue(0));
 		}
 
 		@Override
 		public void review(String exercise) {
 			AbstractServerCall cmd = new ReviewExerciseCommand(exercise, MAGNET, this);
 			cmd.sendRequest();
-			//Proxy.reviewExercise(exercise, MAGNET, this);
 		}
 
 		@Override
@@ -136,8 +228,7 @@ public class ReviewTab extends Composite implements ReviewTabView{
 	public boolean hasPresenter(){
 		return presenter != null;
 	}
-
-
+	
 	@Override
 	public Presenter getPresenter() {
 		return presenter;
@@ -182,6 +273,4 @@ public class ReviewTab extends Composite implements ReviewTabView{
 	public boolean isAdmin() {
 		return true;
 	}
-
-
 }
